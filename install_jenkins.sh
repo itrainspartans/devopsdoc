b7097212946f8ffe36d3e94e8f3fc9f9153176a7
manee2k6 <manee2k6@gmail.com>
2019-09-07 09:03:56 +0530

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,7 +1,9 @@
 #Install Java
 sudo apt-get update
 sudo apt install openjdk-8-jdk -y
+sudo chmod o+w  /etc/environment
 echo JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/environment
+sudo chmod o-w  /etc/environment
 sudo source /etc/environment
 echo $JAVA_HOME
 
@@ -13,8 +15,10 @@ sudo wget https://www-eu.apache.org/dist/maven/maven-3/3.6.1/binaries/apache-mav
 sudo tar -xvzf apache-maven-3.6.1-bin.tar.gz
 sudo mv apache-maven-3.6.1 maven
 sudo rm apache-maven-3.6.1-bin.tar.gz
-sudo vim /etc/profile.d/mavenenv.sh > export M2_HOME=/opt/maven export PATH=${M2_HOME}/bin:${PATH}
-sudo chmod +x /etc/profile.d/mavenenv.sh
+sudo touch /etc/profile.d/mavenenv.sh
+sudo chmod o+w /etc/profile.d/mavenenv.sh
+echo export M2_HOME=/opt/maven export PATH=${M2_HOME}/bin:${PATH} > /etc/profile.d/mavenenv.sh
+sudo chmod ugo+x,o-w /etc/profile.d/mavenenv.sh
 source /etc/profile.d/mavenenv.sh
 mvn --version
 
@@ -27,3 +31,10 @@ sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl status jenkins
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+#Install Openshift Client
+sudo wget https://mirror.openshift.com/pub/openshift-v4/clients/oc/4.1/linux/oc.tar.gz
+sudo tar -xvzf oc.tar.gz
+sudo mv oc /usr/local/bin
+sudo chmod +x /usr/local/bin/oc
+oc version
